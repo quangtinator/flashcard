57bab2ef7e924b600e7cb321e195c4140e338fc3
quangtinator <quang.phung@rwth-aachen.de>
2024-01-12 16:03:21 +0100

    change import

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { css, html } from "lit";
 import { property } from "lit/decorators.js";
 import { customElement } from "lit/decorators.js";
 import { Deck } from "./Deck";
+import { ScopedElementsMixin } from "@open-wc/scoped-elements/html-element.js";
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/input/input.js";
@@ -24,13 +25,19 @@ import "@shoelace-style/shoelace/dist/components/switch/switch.js";
 import "@shoelace-style/shoelace/dist/components/radio-button/radio-button.js";
 import "@shoelace-style/shoelace/dist/components/radio-group/radio-group.js";
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+// import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+import "@shoelace-style/shoelace/dist/components/icon/icon.component.js";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 setBasePath("/node_modules/@shoelace-style/shoelace/dist");
 import mime from "mime";
+import SlIcon from "@shoelace-style/shoelace/dist/components/icon/icon.component.js";
 
 @customElement("ww-flashcards")
-export class WwFlashcards extends LitElementWw {
+export class WwFlashcards extends ScopedElementsMixin(LitElementWw) {
+  static scopedElements = {
+    "sl-icon": SlIcon,
+  };
+
   static styles = css`
     *, html {
       margin: 0;
