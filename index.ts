2cabe76e1ace03828a3eaed338145768e35eb40e
quangtinator <quang.phung@rwth-aachen.de>
2024-01-12 17:59:02 +0100

    test

--- index.ts
+++ index.ts
@@ -25,19 +25,14 @@ import "@shoelace-style/shoelace/dist/components/switch/switch.js";
 import "@shoelace-style/shoelace/dist/components/radio-button/radio-button.js";
 import "@shoelace-style/shoelace/dist/components/radio-group/radio-group.js";
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
-// import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.component.js";
+import "@shoelace-style/shoelace/dist/components/icon/icon.js";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path";
 setBasePath("/node_modules/@shoelace-style/shoelace/dist");
 import mime from "mime";
 import SlIcon from "@shoelace-style/shoelace/dist/components/icon/icon.component.js";
 
 @customElement("ww-flashcards")
-export class WwFlashcards extends ScopedElementsMixin(LitElementWw) {
-  static scopedElements = {
-    "sl-icon": SlIcon,
-  };
-
+export class WwFlashcards extends LitElementWw {
   static styles = css`
     *, html {
       margin: 0;
